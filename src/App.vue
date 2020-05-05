bf14e8adbebad8fcf28f884564603605d47af102
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-05-06 00:49:13 +0300

    event

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <h1>Todo list</h1>
     <hr>
-    <TodoList />
+    <TodoList 
+      v-bind:todos="todos"
+      @remove-todo="removeTodo"
+    />
   </div>
 </template>
 
@@ -12,7 +15,16 @@ export default {
   name: 'App',
   data() {
     return {
-      
+      todos: [
+        {id: 1, title: 'Купить хлеб', completed: false},
+        {id: 2, title: 'Купить масло', completed: false},
+        {id: 3, title: 'Купить молоко', completed: false},
+      ]
+    }
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
     }
   },
   components: {
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,4 +1,50 @@
 <template>
-  <li>Todo item 1</li>
+  <li>
+    <span v-bind:class="{done: todo.completed}">
+      <input type="checkbox"
+        v-on:change="todo.completed = !todo.completed"
+      >
+      <strong>{{todo.id}}</strong>
+      {{todo.title}}
+    </span>
+    <button class="rm"
+      v-on:click="$emit('remove-todo', todo.id)"
+    >&times;</button>
+  </li>
 </template>
 
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  li {
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: space-between;
+    padding: .5rem 2rem;
+    margin-bottom: 1rem;
+  }
+
+  .rm {
+    background: red;
+    color: white;
+    border-radius: 50%;
+    font-weight: bold;
+  }
+
+  input {
+    margin-right: 1rem;
+  }
+
+  .done {
+    text-decoration: line-through;
+  }
+</style>
